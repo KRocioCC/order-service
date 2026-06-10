0d345f9ae92b54c4a4c4f13736b7406defb850b3
Karen Calderon <krcatari@umsa.bo>
2026-06-10 15:55:09 -0400

    implementando models, dtos y mappers

--- src/main/java/com/ecommerce/order_service/mapper/OrderMapper.java
+++ src/main/java/com/ecommerce/order_service/mapper/OrderMapper.java
@@ -1,4 +1,31 @@
 package com.ecommerce.order_service.mapper;
 
-public class OrderMapper {
+import com.ecommerce.order_service.dto.OrderLineItemsRequest;
+import com.ecommerce.order_service.dto.OrderLineItemsResponse;
+import com.ecommerce.order_service.dto.OrderRequest;
+import com.ecommerce.order_service.dto.OrderResponse;
+import com.ecommerce.order_service.model.Order;
+import com.ecommerce.order_service.model.OrderLineItems;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+
+    // De Request a Entidad
+    // Mapeamos ls lista porque los nombres no coinciden
+//    @Mapping(source = "orderLineItemsDtoList", target = "orderLineItemsList")
+    Order toOrder(OrderRequest orderRequest);
+
+    // metodo aux
+    OrderLineItems toOrderLineItems(OrderLineItemsRequest orderLineItemsRequest);
+
+
+    // De Entidad a Response
+    // Mapeamos  la lista de vuelta
+//    @Mapping(source = "orderLineItemsList", target = "orderLineItemsDtoList")
+    OrderResponse toOrderResponse(Order order);
+
+    // metodo aux
+    OrderLineItemsResponse toOrderLineItemsResponse(OrderLineItems orderLineItems);
 }
