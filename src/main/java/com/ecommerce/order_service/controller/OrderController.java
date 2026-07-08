22a6dcacb3e025ea73325aa49d7bf5332f1b2244
Karen Calderon <krcatari@umsa.bo>
2026-07-08 09:07:09 -0400

    cambiando logica de endpoint  para que un usuario pueda ver solo sus ordenes

--- src/main/java/com/ecommerce/order_service/controller/OrderController.java
+++ src/main/java/com/ecommerce/order_service/controller/OrderController.java
@@ -6,9 +6,12 @@ import com.ecommerce.order_service.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -17,17 +20,34 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    // PARA CREAR UNA ORDEN, SE USA EL JWT PARA OBTENER EL USERID Y SE PASA AL SERVICIO PARA CREAR LA ORDEN CON ESE USERID
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public OrderResponse placeOrder(@Valid @RequestBody OrderRequest orderRequest) {
-        return orderService.placeOrder(orderRequest);
+    public OrderResponse placeOrder(@Valid @RequestBody OrderRequest orderRequest,
+                                    @AuthenticationPrincipal Jwt jwt
+    ) {
+        return orderService.placeOrder(orderRequest, jwt.getSubject());
     }
 
+    // PARA OBTENER TODAS LAS ORDENES DE UN USUARIO, SE USA EL JWT PARA OBTENER EL USERID Y SE PASA AL SERVICIO PARA FILTRAR LAS ORDENES POR ESE USERID
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<OrderResponse> getAllOrders() {
-        return orderService.getAllOrders();
+    public List<OrderResponse> getOrders(@AuthenticationPrincipal Jwt jwt) {
+        String userId = jwt.getSubject();
+
+        boolean isAdmin = false;
+
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if (realmAccess != null && realmAccess.containsKey("roles")) {
+            List<String> roles = (List<String>) realmAccess.get("roles");
+            isAdmin = roles.stream()
+                    .anyMatch(role -> role.equalsIgnoreCase("ADMIN"));
+        }
+
+        return orderService.getOrders(userId, isAdmin);
     }
+
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
